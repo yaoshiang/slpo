fixed calculation of y and y_bar to correct grad flow.

src/slpo/slpo.py
@@ -19,12 +19,30 @@ from torch.nn import functional as F
 from torch.nn.modules.loss import _Loss
 
 
+def _logdiffexp(t1, t2):
+    """Helper for log diff exp on two tensors.
+
+    This function computes log(exp(t1) - exp(t2)) in a stable way.
+    """
+    assert t1.size() == t2.size(), f"{t1.size()} != {t2.size()}"
+    if torch.any(t1 < t2):
+        raise ValueError("t1 must be greater than t2.")
+
+    retval = t1 + torch.log1p(-torch.exp(t2 - t1))
+
+    # Look for -inf in t1 and t2 and manually patch the result to -inf.
+    retval[(t1 == float("-inf")) & (t2 == float("-inf"))] = float("-inf")
+
+    return retval
+
+
 def _compute_logprob_y_bar_y(
     logprobs: torch.Tensor, y_tokens: torch.Tensor
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
     Given:
-      logprobs: shape (T, V) = log softmax outputs for a single sequence.
+      logprobs: shape (T, V) = log softmax outputs for a single sequence,
+        where T is timestep (eg sequence length, not tokens), and V is vocab size.
       y_tokens: shape (T,) = sequence of token-IDs.
 
     Returns:
@@ -38,8 +56,10 @@ def _compute_logprob_y_bar_y(
     T, _ = logprobs.shape  # T = sequence length, _ = vocab size
 
     # Logprobs should sum to log(1.0) = 0.0 for each time step t.
-    logprob_t = torch.logsumexp(logprobs, dim=-1)
+    logprob_t = torch.logsumexp(logprobs, dim=-1)  # Shape (T,)
+    assert logprob_t.size() == (T,)
     assert torch.allclose(logprob_t, torch.zeros_like(logprob_t), atol=1e-3)
+    del logprob_t
 
     # Edge case: Handle empty sequences
     if T == 0:
@@ -47,34 +67,46 @@ def _compute_logprob_y_bar_y(
 
     # 1) Extract log probability of the chosen tokens
     logprob_y_t = logprobs[torch.arange(T), y_tokens]
-    logprob_y = logprob_y_t.sum()  # Log of joint prob of y seq over steps t.
+    logprob_y = (
+        logprob_y_t.sum()
+    )  # Multiplication to get joint prob, in log space.
 
-    # 2) Compute sigma sums for "first mismatch" trick
-    logprob_sigma_y_lt_t_shifted = torch.cat(
+    # 2) Compute log prob of the non-y sequences. Basically, we want to calculate:
+    #    a) prob(y_bar_t1), plus
+    #    b) prob(y_t1, y_bar_t2) # Which automatically captures t2 being EOS, and, all other values of t2 that then flow to t3.
+    #    c) prob(y_t1, y_t2, y_bar_t3)
+    #    d) etc.
+    #
+    # 2a) First, calculate the odds of the first tokens starting with y. The base
+    #     case starts with "100%" (log(1)=0) to make the math work. The indexes
+    #     are shifted, since for a sequence of length n, we would have n-1 y tokens and 1 bar_y token.
+    logprob_y_until_t = torch.cat(
         [
-            torch.tensor(
-                [0.0], device=logprobs.device, dtype=logprobs.dtype
-            ),  # Initial zero for sigma sum
-            torch.cumsum(logprob_y_t, dim=0),  # Shape (T)
+            torch.tensor([0.0], device=logprobs.device, dtype=logprobs.dtype),
+            torch.cumsum(logprob_y_t[:-1], dim=-1),
         ]
-    )  # Shape (T+1)
-    logprob_sigma_y_lt_t = logprob_sigma_y_lt_t_shifted[:-1]  # Shape (T,)
-    del logprob_sigma_y_lt_t_shifted  # Free memory
+    )  # Shape (T,)
 
-    assert logprob_sigma_y_lt_t.size() == (T,)
+    assert logprob_y_until_t.size() == (T,)
 
-    # 3) Compute log probability of any incorrect token at each step
-    # Better to not use torch.log1p(-torch.exp(logprob_y_t)).
-    # Uncertain if the gradients will be correct with that shortcut
-    # given log_softmax layer. Instead, clone and mask y_t to -inf.
-    # logsumexp will handle -inf values correctly.
-    logprob_masked_y_t = logprobs.clone()
-    logprob_masked_y_t[torch.arange(T), y_tokens] = -float("inf")
-    logprob_bar_y_t = torch.logsumexp(logprob_masked_y_t, dim=-1)
+    # 2b) Compute sum of the y_bar_t
+    #     Calculate the sum of all possible vocabs (100%), minus the y_t.
+    logprob_t = torch.logsumexp(logprobs, dim=-1)  # Shape (T,)
+    logprob_y_bar_t = _logdiffexp(logprob_t, logprob_y_t)  # Shape (T,)
 
-    # 5) Compute the mismatch log probability
-    logprob_not_y = torch.logsumexp(
-        logprob_sigma_y_lt_t + logprob_bar_y_t, dim=0
+    # 2c) Fuse the first half (y_t1..n-1) and second half (y_bar_tn) for all sequence locations.
+    #     This is a joint prob, so we sum directly.
+    logprob_not_y_through_t = logprob_y_until_t + logprob_y_bar_t  # Shape (T,)
+
+    # 2d) Sum over all possible locations where the first y_bar token is at time step t.
+    logprob_not_y = torch.logsumexp(logprob_not_y_through_t, dim=-1)  # Scalar
+
+    assert (
+        torch.logsumexp(torch.stack([logprob_y, logprob_not_y]), -1) > -0.000001
+    ), (
+        f"Expected logsumexp(logprob_y, logprob_not_y) to be 0.0, got \n"
+        f"{torch.logsumexp(torch.stack([logprob_y, logprob_not_y]), -1)}, \n"
+        f"{logprob_y},  \n{logprob_not_y}"
     )
 
     return logprob_y, logprob_not_y
@@ -110,16 +142,20 @@ def slpo_loss(input: torch.Tensor, target: dict) -> torch.Tensor:
     # Convert to log-probs
     logprobs = F.log_softmax(input, dim=-1)  # shape (S, V)
 
-    if target["winner"]:
+    if target["winner"].item():
         w_w = target["pi_ref_w"] + target["pi_ref_l"]
 
         # Get p_theta(y_w) and p_theta(\overline{y_w})
-        log_p_y, log_p_bar_y = _compute_logprob_y_bar_y(logprobs, target["y"])
-        loss = -(w_w * log_p_y + (1.0 - w_w) * log_p_bar_y)
+        logprob_y, logprob_bar_y = _compute_logprob_y_bar_y(
+            logprobs, target["y"]
+        )
+        loss = -(w_w * logprob_y + (1.0 - w_w) * logprob_bar_y)
     else:
         # Get p_theta(y_l) and p_theta(\overline{y_l})
-        log_p_y, log_p_bar_y = _compute_logprob_y_bar_y(logprobs, target["y"])
-        loss = -(0.0 * log_p_y + 1.0 * log_p_bar_y)
+        logprob_y, logprob_bar_y = _compute_logprob_y_bar_y(
+            logprobs, target["y"]
+        )
+        loss = -(0.0 * logprob_y + 1.0 * logprob_bar_y)
 
     return loss
 

test/ml/test_ml_slpo.py
@@ -1,7 +1,8 @@
+import pytest
 import torch
+from test_utils import memorization_model
 
 from slpo import slpo
-from test_utils import memorization_model
 
 
 class DictDataset(
@@ -26,7 +27,7 @@ def test_slpo_batch_size_two():
 
     optimizer = torch.optim.AdamW(model.parameters(), lr=0.1)
     criterion = slpo.SLPO()
-    target = [
+    targets = [
         {
             "pi_ref_w": torch.tensor(0.02),
             "pi_ref_l": torch.tensor(0.05),
@@ -40,7 +41,7 @@ def test_slpo_batch_size_two():
             "winner": torch.tensor(False, dtype=torch.bool),
         },
     ]
-    dataset = DictDataset(torch.ones((2,)), target)
+    dataset = DictDataset(torch.ones((2,)), targets)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=2)
 
     # Act
@@ -52,13 +53,61 @@ def test_slpo_batch_size_two():
             loss.backward()
             optimizer.step()
 
-    # Assert
-    joint_prob_0 = model.joint_prob(0, target["y"][0])
-    joint_prob_1 = model.joint_prob(1, target["y"][1])
+    joint_prob_0 = model.joint_prob(0, targets[0]["y"])
+    joint_prob_1 = model.joint_prob(1, targets[1]["y"])
 
+    # Assert
     torch.testing.assert_close(
         joint_prob_0, torch.tensor(0.07), atol=0.01, rtol=0.1
     )  # y_ref_w + y_ref_l: 0.02 + 0.05
     torch.testing.assert_close(
         joint_prob_1, torch.tensor(0.0), atol=0.01, rtol=0.1
     )  # This is the expected value for the losing sequence.
+
+
+@pytest.mark.parametrize("seq_length", [5, 128])
+@pytest.mark.parametrize("vocab_size", [7, 1000])
+def test_slpo_batch_many_negatives(seq_length, vocab_size):
+    # Arrange
+    model = memorization_model.MemorizationModel(
+        batch_size=2, seq_len=seq_length, vocab_size=vocab_size
+    )
+
+    optimizer = torch.optim.SGD(
+        model.parameters(), lr=1.0
+    )  # High learning rate - we are testing blasting down the loser to zero.
+    criterion = slpo.SLPO()
+    targets = [
+        {
+            "pi_ref_w": torch.tensor(0.02),
+            "pi_ref_l": torch.tensor(0.05),
+            "y": torch.arange(seq_length) % vocab_size,
+            "winner": torch.tensor(True, dtype=torch.bool),
+        },
+        {
+            "pi_ref_w": torch.tensor(0.03),
+            "pi_ref_l": torch.tensor(0.07),
+            "y": torch.arange(seq_length) % vocab_size,
+            "winner": torch.tensor(False, dtype=torch.bool),
+        },
+    ]
+    dataset = DictDataset(torch.ones((2,)), targets)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=2)
+
+    ref_joint_prob_1 = model.joint_log_prob(1, targets[1]["y"])
+
+    # Act
+    for epoch in range(100):
+        for batch in dataloader:
+            input, target = batch
+            optimizer.zero_grad()
+            loss = criterion(model(input), target)
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+    # Assert
+    joint_prob_1 = model.joint_log_prob(1, targets[1]["y"])
+
+    assert joint_prob_1 < ref_joint_prob_1 or ref_joint_prob_1 < -25, (
+        f"{joint_prob_1=}, {ref_joint_prob_1=}"
+    )

test/unit/test_slpo.py
@@ -1,39 +1,71 @@
-import pytest
 import torch
-from test_utils.memorization_model import MemorizationModel
 
-from slpo.slpo import _compute_logprob_y_bar_y
+from slpo.slpo import _compute_logprob_y_bar_y, _logdiffexp, slpo_loss
 
 
-@pytest.fixture
-def logits():
-    retval = torch.tensor(
+def test_logdiffexp_corners():
+    # Arrange
+    t1 = torch.log(torch.tensor([0.0, 1.0, 0.1, 1.0]))
+    t2 = torch.log(torch.tensor([0.0, 1.0, 0.1, 0.0]))
+
+    expected = torch.tensor([float("-inf"), float("-inf"), float("-inf"), 0.0])
+
+    # Act
+    result = _logdiffexp(t1, t2)
+
+    # Assert
+    assert torch.allclose(expected, result, atol=1e-6)
+
+
+def test_logdiffexp():
+    # Arrange
+    t1 = torch.log(torch.tensor([0.10, 0.45, 0.9]))
+    t2 = torch.log(torch.tensor([0.05, 0.21, 0.0]))
+
+    expected = torch.log(torch.exp(t1) - torch.exp(t2))
+
+    # Act
+    result = _logdiffexp(t1, t2)
+
+    # Assert
+    assert torch.allclose(expected, result, atol=1e-6)
+
+
+def test_compute_logprob_y_bar_y_minimal():
+    # Arrange
+    log_probs = torch.stack(
         [
-            [
-                [1.0, 2.0, 3.0],
-                [2.0, 4.0, 6.0],
-                [3.0, 9.0, 12.0],
-            ],
-            [
-                [3.0, 2.0, 1.0],
-                [0.0, -0.5, -1.0],
-                [-1.0, -1.25, -1.5],
-            ],
+            torch.log_softmax(torch.tensor([0.0, 0.0]), -1),
+            torch.log_softmax(torch.tensor([0.0, 0.0]), -1),
         ]
     )
-    return retval
+    y_tokens = torch.tensor([0, 1])
+    expected_log_p_y = torch.log(torch.tensor(0.25))
+    expected_log_p_not_y = torch.log(torch.tensor(0.75))
+
+    # Act
+    log_p_y, log_p_not_y = _compute_logprob_y_bar_y(log_probs, y_tokens)
+
+    # Assert
+    assert torch.isclose(log_p_y, expected_log_p_y, atol=1e-6), (
+        f"Expected log_p_y={expected_log_p_y}, got {log_p_y}"
+    )
+
+    assert torch.isclose(log_p_not_y, expected_log_p_not_y, atol=1e-6), (
+        f"Expected log_p_not_y={expected_log_p_not_y}, got {log_p_not_y}"
+    )
+
+    # Ensure log_p_y + log_p_not_y ~= 100% (valid probability distribution)
+    assert torch.isclose(
+        torch.tensor(1.0),
+        torch.exp(log_p_y) + torch.exp(log_p_not_y),
+        atol=1e-6,
+    ), (
+        f"log_p_y ({torch.exp(log_p_y)}) + log_p_not_y ({torch.exp(log_p_not_y)}) should ~= 100%."
+    )
 
 
-@pytest.fixture
-def model(logits):
-    """Fixture to create a simple model for testing."""
-    retval = MemorizationModel(num_examples=2, seq_len=3, vocab_size=3)
-    retval.logits.data = logits
-    return retval
-    # Set the logits to a known value for reproducibility.
-
-
-def test_compute_log_p_and_log_p_not_y():
+def test_compute_logprob_y_bar_y():
     # Arrange
     log_probs = torch.stack(
         [
@@ -64,7 +96,7 @@ def test_compute_log_p_and_log_p_not_y():
         f"Expected log_p_not_y={expected_log_p_not_y}, got {log_p_not_y}"
     )
 
-    # Ensure log_p_y is always greater than log_p_not_y (valid probability distribution)
+    # Ensure log_p_y + log_p_not_y ~= 100% (valid probability distribution)
     assert torch.isclose(
         torch.tensor(1.0),
         torch.exp(log_p_y) + torch.exp(log_p_not_y),
@@ -74,8 +106,8 @@ def test_compute_log_p_and_log_p_not_y():
     )
 
 
-# Test that logsoftmax can handle -inf values.
 def test_functional_inf():
+    """Test that logsoftmax can handle -inf values."""
     # Arrange
     x = torch.tensor([[0.0, 0.0, -float("inf")]])
     y_expected = torch.tensor(
@@ -95,9 +127,55 @@ def test_functional_inf():
     assert torch.allclose(y_expected, y_true, atol=1e-6)
 
 
-# Test that y_w and y_l will be close to our target value after optimization.
-# def test_slpo_training(model):
-#     opt = torch.optim.AdamW(model.parameters(), lr=0.1)
+def test_slpo_grads_loser():
+    # Arrange
+    output = torch.ones((2, 3), requires_grad=True)
+    target = {
+        "pi_ref_w": torch.tensor(0.03),
+        "pi_ref_l": torch.tensor(0.07),
+        "y": torch.tensor([1, 0]),
+        "winner": torch.tensor(False, dtype=torch.bool),
+    }
+
+    # Act
+    loss = slpo_loss(output, target)
+    loss.backward()
+
+    # Assert
+
+    # These are the y_l tokens, so the gradients should be positive (less weight on loser).
+    assert output.grad[0, 1] > 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 0] > 0.0, f"output.grad={output.grad}"
+
+    # These are the \overline{y_l} tokens, so the gradients should be negative.
+    assert output.grad[0, 0] < 0.0, f"output.grad={output.grad}"
+    assert output.grad[0, 2] < 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 1] < 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 2] < 0.0, f"output.grad={output.grad}"
 
 
-#     for epoch in range(100):
+def test_slpo_grads_winner():
+    # Arrange
+    output = torch.ones((2, 3), requires_grad=True)
+    target = {
+        "pi_ref_w": torch.tensor(0.1),
+        "pi_ref_l": torch.tensor(0.1),
+        "y": torch.tensor([1, 0]),
+        "winner": torch.tensor(True, dtype=torch.bool),
+    }
+
+    # Act
+    loss = slpo_loss(output, target)
+    loss.backward()
+
+    # Assert
+
+    # These are the y_w tokens, so the grads should be neg (more weight on winner).
+    assert output.grad[0, 1] > 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 0] > 0.0, f"output.grad={output.grad}"
+
+    # These are the \overline{y_w} tokens, so the grads should be positive (less weight on winner).
+    assert output.grad[0, 0] > 0.0, f"output.grad={output.grad}"
+    assert output.grad[0, 2] > 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 1] > 0.0, f"output.grad={output.grad}"
+    assert output.grad[1, 2] > 0.0, f"output.grad={output.grad}"