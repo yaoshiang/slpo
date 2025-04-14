36b7bb67d821d0d50b6d0d7e8770241bb0544e4a
Yaoshiang Ho <yaoshiang@gmail.com>
2025-04-14 02:17:45 +0000

    typo

---
+++ src/experiment/sentiment/eval_base_phi2.py
@@ -0,0 +1,17 @@
+"""Evaluates phi2 on the sentiment dataset."""
+
+import sys
+
+import transformers
+
+def main() -> int:
+    """Main function to evaluate base phi2 on the sentiment dataset.
+    
+    Returns:
+        int: zero if no errors.
+    """
+
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
